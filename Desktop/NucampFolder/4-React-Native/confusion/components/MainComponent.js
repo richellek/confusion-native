ab94281369af37d519dc44dcd67b0178a2932077
Richelle Kikuji <richellek@ctk.church>
2019-05-03 10:58:00 -0700

    Navigation Part 2

--- Desktop/NucampFolder/4-React-Native/confusion/components/MainComponent.js
+++ Desktop/NucampFolder/4-React-Native/confusion/components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+
 
 const MenuNavigator = createStackNavigator({
         Menu: { screen: Menu },
@@ -21,14 +23,43 @@ const MenuNavigator = createStackNavigator({
     }
 );
 
-class Main extends Component {
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+    }, {
+        navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+            color: "#fff"
+        }
+        }
+});
 
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+        },
+        Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        }
+        }
+    }, {    drawerBackgroundColor: '#D1C4E9'});
+
+class Main extends Component {
     render() {
-        
-        return (
-            <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <MenuNavigator />
-            </View>
+        return(
+        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+            <MainNavigator />
+        </View>
         );
     }
 }
