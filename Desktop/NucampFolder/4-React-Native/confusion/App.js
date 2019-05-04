5f71e295d48a1eeed419aee6a93ae18d3fd6ae78
Richelle Kikuji <richellek@ctk.church>
2019-05-04 11:42:14 -0700

    Week Assignment

--- Desktop/NucampFolder/4-React-Native/confusion/App.js
+++ Desktop/NucampFolder/4-React-Native/confusion/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './components/MainComponent';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default class App extends React.Component {
   render() {
--- Desktop/NucampFolder/4-React-Native/confusion/components/MainComponent.js
+++ Desktop/NucampFolder/4-React-Native/confusion/components/MainComponent.js
@@ -1,65 +1,129 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishDetailComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
-
-const MenuNavigator = createStackNavigator({
-        Menu: { screen: Menu },
-        DishDetail: { screen: DishDetail }
-    }, {
-        initialRouteName: 'Menu',
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
         navigationOptions: {
             headerStyle: {
-                backgroundColor: "#512DA8"
+                backgroundColor: '#512DA8'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
-                color: "#fff"            
+                color: '#fff'
             }
         }
     }
 );
 
-const HomeNavigator = createStackNavigator({
-    Home: { screen: Home }
-    }, {
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
         navigationOptions: {
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-            color: "#fff"
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
         }
-        }
-});
+    }
+);
 
-const MainNavigator = createDrawerNavigator({
-    Home: {
-        screen: HomeNavigator,
+const MenuNavigator = createStackNavigator(
+    {
+        Menu: { screen: Menu },
+        DishDetail: { screen: DishDetail }
+    },
+    {
+        initialRouteName: 'Menu',
         navigationOptions: {
-            title: 'Home',
-            drawerLabel: 'Home'
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
         }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                title: 'Home',
+                drawerLabel: 'Home'
+            }
+        },
+        About: {
+            screen: AboutNavigator,
+            navigationOptions: {
+                title: 'About Us',
+                drawerLabel: 'About Us'
+            }
         },
         Menu: {
-        screen: MenuNavigator,
-        navigationOptions: {
-            title: 'Menu',
-            drawerLabel: 'Menu'
+            screen: MenuNavigator,
+            navigationOptions: {
+                title: 'Menu',
+                drawerLabel: 'Menu'
+            }
+        },
+        Contact: {
+            screen: ContactNavigator,
+            navigationOptions: {
+                title: 'Contact Us',
+                drawerLabel: 'Contact Us'
+            }
         }
-        }
-    }, {    drawerBackgroundColor: '#D1C4E9'});
+    },
+    { drawerBackgroundColor: '#D1C4E9' }
+);
 
 class Main extends Component {
     render() {
-        return(
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-            <MainNavigator />
-        </View>
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    paddingTop:
+                        Platform.OS === 'ios'
+                            ? 0
+                            : Expo.Constants.statusBarHeight
+                }}>
+                <MainNavigator />
+            </View>
         );
     }
 }
