5c39a0b08473f0545c64906e09bc3b8629165cb1
Richelle Kikuji <richellek@ctk.church>
2019-05-16 13:08:02 -0700

    Gestures part 1

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 
