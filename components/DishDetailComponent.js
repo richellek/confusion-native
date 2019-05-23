37f1bbde0394d904d4e5caf4c506829980f75ff8
Richelle Kikuji <richellek@ctk.church>
2019-05-23 13:00:24 -0700

    social sharing

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -69,6 +69,19 @@ function RenderDish(props) {
         }
     });
 
+    const shareDish = (title, message, url) => {
+        Share.share(
+            {
+                title: title,
+                message: title + ': ' + message + ' ' + url,
+                url: url
+            },
+            {
+                dialogTitle: 'Share ' + title
+            }
+        );
+    };
+
     if (dish != null) {
         return (
             <Animatable.View animation='fadeInDown' duration={2000} delay={1000} ref={this.handleViewRef} {...panResponder.panHandlers}>
@@ -84,6 +97,15 @@ function RenderDish(props) {
                             onPress={() => (props.favorite ? console.log('Already favorite') : props.onPress())}
                         />
                         <Icon raised reverse name={'pencil'} type='font-awesome' color='#4250f4' onPress={props.onToggleModal} />
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)}
+                        />
                     </View>
                 </Card>
             </Animatable.View>
